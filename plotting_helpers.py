712d4963488682aca18a6744bffa4008a4863e5b
Hiro Josep Farre Kaga <hf8585@traverse.princeton.edu>
2023-11-04 12:27:19 -0400

    restructure modelRollout to use prediction_helpers for prediction functions, plotting_helpers for plotting functions. Add flag to run modelRollout with fake actuators

---
+++ plotting_helpers.py
@@ -0,0 +1,73 @@
+import matplotlib.pyplot as plt
+from matplotlib import cm
+import torch
+import dataSettings
+import numpy as np
+
+label_map={'zipfit_etempfit_rho': r'$T_e$',
+           'zipfit_itempfit_rho': r'$T_i$',
+           'zipfit_edensfit_rho': r'$n_e$',
+           'zipfit_trotfit_rho': r'$\Omega$',
+           'qpsi_EFIT01': r'$q$',
+           'pinj': r'$P_{NBI}$',
+           'tinj': r'$T_{NBI} (N m)$',
+           'ip': r'$I_p$',
+           'bt': r'$B_t$',
+           'Ar_tot': 'Ar',
+           'D_tot': 'D',
+           'H_tot': 'H',
+           'He_tot': 'He',
+           'N_tot': 'N',
+           'Ne_tot': 'Ne',
+           'li_EFIT01': 'li',
+           'tribot_EFIT01': r'$\delta_l$',
+           'tritop_EFIT01': r'$\delta_u$',
+           'kappa_EFIT01': r'$\kappa$',
+           'volume_EFIT01': 'V',
+           'ipsiptargt': r'$I_p^{target}$',
+           'dssdenest': r'$<n_e>$'}
+
+def modelRollout_plot(predicted_means, predicted_stds, denormalized_true_dic, plotted_times, time_length, plotted_profiles, plotted_parameters, plotted_actuators, rho_ind, shot, title):
+    with torch.no_grad():
+        NSTEPS_PLOTTED=3
+        num_columns = 3
+        if len(plotted_parameters)>0:
+            num_columns = 4
+        fig,axes=plt.subplots(max(len(plotted_profiles),len(plotted_parameters),len(plotted_actuators)),num_columns, sharex='col', figsize=(8,5))
+        plt.subplots_adjust(hspace=0, wspace=1)
+        colors=cm.viridis(np.linspace(0,1,NSTEPS_PLOTTED+1))
+        plotted_time_inds=[int(t) for t in np.linspace(0, time_length, NSTEPS_PLOTTED, endpoint=False)]
+        x=np.linspace(0,1,dataSettings.nx)
+        with torch.no_grad():
+            #plotted_profiles[0], plotted_profiles[3] = plotted_profiles[3], plotted_profiles[0]
+            #plotted_actuators.insert(0, plotted_actuators.pop())
+            for i,profile in enumerate(plotted_profiles):
+                axes[i,1].errorbar(plotted_times, predicted_means[profile][:,rho_ind], yerr=predicted_stds[profile][:,rho_ind],
+                                   label='predicted', c='k', alpha=0.1)
+                axes[i,1].plot(plotted_times, denormalized_true_dic[profile][:,rho_ind],
+                               label='real', c='k', linestyle='--')
+                axes[i,1].set_ylabel(label_map[profile])
+                for which_color, time_ind in enumerate(plotted_time_inds):
+                    axes[i,2].plot(x, predicted_means[profile][time_ind], c=colors[which_color],
+                                   label=f'{plotted_times[time_ind]}ms')
+                    axes[i,2].plot(x, denormalized_true_dic[profile][time_ind],
+                                   linestyle='--', c=colors[which_color])
+            for i,actuator in enumerate(plotted_actuators):
+                axes[i,0].plot(plotted_times, denormalized_true_dic[actuator][0],
+                               label='real', c='k', linestyle='--')
+                axes[i,0].set_ylabel(label_map[actuator])
+            if len(plotted_parameters)>0:
+                for i,parameter in enumerate(plotted_parameters):
+                    axes[i,3].errorbar(plotted_times, predicted_means[parameter][:, 0], yerr=predicted_stds[parameter][:, 0],
+                                   label='predicted', c='k', alpha=0.1)
+                    axes[i,3].plot(plotted_times, denormalized_true_dic[parameter],
+                                   label='real', c='k', linestyle='--')
+                    axes[i,3].set_ylabel(label_map[parameter])
+            axes[0, 0].text(0.5, 1.05, 'Actuators over time', transform=axes[0, 0].transAxes, fontsize=10, ha='center')
+            axes[0, 1].text(0.5, 1.05, 'Predictions over time', transform=axes[0, 1].transAxes, fontsize=10, ha='center')
+            axes[0, 2].text(0.5, 1.05, 'Predicted profiles', transform=axes[0, 2].transAxes, fontsize=10, ha='center')
+        axes[0, 1].legend(loc='center left', bbox_to_anchor=(1, 0.5), fontsize=6)
+        axes[1, 2].legend(fontsize=6)
+        fig.suptitle(f'{title}')
+        plt.savefig(f'plots/{title}.svg')
+        plt.show()
---
+++ prediction_helpers.py
@@ -0,0 +1,128 @@
+import configparser
+import torch
+#torch.manual_seed(0)
+import dataSettings
+import numpy as np
+import os
+import glob
+from customModels import IanRNN, IanMLP, HiroLinear
+
+models={'IanRNN': IanRNN, 'IanMLP': IanMLP, 'HiroLinear': HiroLinear}
+
+@torch.no_grad()
+class ModelStepper:
+    def __init__(self, initial_state, considered_models, profiles, parameters):
+        num_model_options=0
+        self.profiles=profiles
+        self.parameters=parameters
+        self.models=considered_models
+        state_length=len(profiles)*dataSettings.nx+len(parameters)
+        self.all_predictions=torch.zeros((len(self.models),state_length))
+        for which_model,model in enumerate(self.models):
+            self.all_predictions[which_model]=initial_state
+    def prediction_step(self, actuator_array):
+        for which_model,model in enumerate(self.models):
+            input_tensor=torch.cat((self.all_predictions[which_model],actuator_array))
+            self.all_predictions[which_model]=model(input_tensor[None,:])[0]
+    def get_denormed_predictions(self):
+        normed_dic=dataSettings.state_to_dic(self.all_predictions, profiles=self.profiles, parameters=self.parameters)
+        for sig in normed_dic:
+            normed_dic[sig]=torch.stack(normed_dic[sig])
+        return dataSettings.get_denormalized_dic(normed_dic)
+
+def get_considered_models(config_filename, plot_ensemble=True):
+    config=configparser.ConfigParser()
+    config.read(config_filename)
+    output_filename_base=config['model']['output_filename_base']
+    output_dir=config['model']['output_dir']
+    model_type=config['model']['model_type']
+    profiles=config['inputs']['profiles'].split()
+    actuators=config['inputs']['actuators'].split()
+    parameters=config['inputs']['parameters'].split()
+    state_length=len(profiles)*dataSettings.nx+len(parameters)
+    actuator_length=len(actuators)
+    considered_models=[]
+    if plot_ensemble:
+        all_model_files=glob.glob(os.path.join(output_dir, f'{output_filename_base}*.tar'))
+        # exclude models under the median loss
+        losses = []
+        for model_file in all_model_files:
+            saved_state=torch.load(model_file, map_location=torch.device('cpu'))
+            losses.append(np.min([saved_state['val_losses'][-i] for i in range(10)]))
+        max_loss = np.median(losses)
+        for model_file in all_model_files:
+            saved_state=torch.load(model_file, map_location=torch.device('cpu'))
+            if saved_state['val_losses'][-1]<max_loss:
+                model=models[model_type](input_dim=state_length+2*actuator_length, output_dim=state_length,
+                                         **saved_state['model_hyperparams'])
+                model.load_state_dict(saved_state['model_state_dict'])
+                considered_models.append(model)
+        print(f'{len(considered_models)}/{len(all_model_files)} models used (i.e. only loss<{max_loss})')
+    else:
+        model_file=os.path.join(output_dir, f'{output_filename_base}.tar')
+        saved_state=torch.load(model_file, map_location=torch.device('cpu'))
+        model=models[model_type](input_dim=state_length+2*actuator_length, output_dim=state_length,
+                                 **saved_state['model_hyperparams'])
+        model.load_state_dict(saved_state['model_state_dict'])
+        considered_models=[model]
+    return considered_models
+
+def get_predictions(normalized_true_state, considered_models, profiles, parameters):
+    time_length=len(normalized_true_state)
+    state_length=len(profiles)*dataSettings.nx+len(parameters)
+    predicted_means={}
+    predicted_stds={}
+    for profile in profiles:
+        predicted_means[profile]=torch.zeros((time_length,dataSettings.nx))
+        predicted_stds[profile]=torch.zeros((time_length,dataSettings.nx))
+    for parameter in parameters:
+        predicted_means[parameter]=torch.zeros((time_length,1))
+        predicted_stds[parameter]=torch.zeros((time_length,1))
+    for step in range(time_length):
+        step_tensor=normalized_true_state[step]
+        step_state=step_tensor[:state_length]
+        step_actuators=step_tensor[state_length:]
+        if step==0:
+            modelstepper=ModelStepper(step_state, considered_models, profiles, parameters)
+        modelstepper.prediction_step(step_actuators)
+        denormed_predictions=modelstepper.get_denormed_predictions()
+        for sig in profiles+parameters:
+            predicted_means[sig][step, :]=torch.mean(denormed_predictions[sig], dim=0)
+            predicted_stds[sig][step, :]=torch.std(denormed_predictions[sig], dim=0)
+    return predicted_means, predicted_stds
+
+def get_predictions_per_model(normalized_true_state, considered_models, profiles, parameters):
+    time_length=len(normalized_true_state)
+    state_length=len(profiles)*dataSettings.nx+len(parameters)
+    predicted_values = {}
+    for profile in profiles:
+        predicted_values[profile]=torch.zeros((time_length,len(considered_models),dataSettings.nx))
+    for parameter in parameters:
+        predicted_values[parameter]=torch.zeros((time_length,len(considered_models),1))
+    for step in range(time_length):
+        step_tensor=normalized_true_state[step]
+        step_state=step_tensor[:state_length]
+        step_actuators=step_tensor[state_length:]
+        if step==0:
+            modelstepper=ModelStepper(step_state, considered_models, profiles, parameters)
+        modelstepper.prediction_step(step_actuators)
+        denormed_predictions=modelstepper.get_denormed_predictions()
+        for sig in profiles+parameters:
+            predicted_values[sig][step, :, :]=denormed_predictions[sig]
+    return predicted_values
+
+def get_fake_actuator_state(normalized_true_state, profiles, parameters, actuators):
+    fake_actuator_state=normalized_true_state.clone()
+    fake_actuator_dic=dataSettings.state_to_dic(fake_actuator_state, profiles=profiles, parameters=parameters, actuators=actuators)
+    for actuator in actuators:
+        arr = fake_actuator_dic[actuator][0]
+        freeze_index = len(arr)//2 - 40
+        perturb_index = len(arr)//2 + 30
+        perturb_length = 20
+        arr[freeze_index:-1] = torch.tensor([arr[freeze_index]]*len(arr[freeze_index:-1]))
+        if (actuator=='D_tot'):
+            perturb_index = len(arr)//2 + 30
+            perturb_length = 30
+            arr[perturb_index:perturb_index + perturb_length] = torch.tensor([((np.sin(np.pi*i/perturb_length))*arr[perturb_index] + arr[perturb_index]) for i in range(perturb_length)])
+    fake_actuator_state = dataSettings.dic_to_state(fake_actuator_dic, profiles, parameters, actuators=actuators)
+    return fake_actuator_state
