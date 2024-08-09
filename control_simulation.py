ebd62825f656f7bc6204002d01f214b7d93540dd
HiroFarre <hiro.farre@gmail.com>
2024-08-09 14:32:20 -0400

    fix control_simulation off by 1 errors, improve save file formatting

--- control_simulation.py
+++ control_simulation.py
@@ -13,9 +13,12 @@ import prediction_helpers
 import osqp
 import scipy as sp
 from scipy import sparse
+from prediction_helpers import get_ml_truth, get_ml_profile_warmup, get_ml_actuator_trajectory, get_ml_predictions, get_considered_models, get_fast_profile_prediction
+
 
 
 lstm_model_name = 'HiroLRAN_betan6'
+#lstm_model_name = 'HiroLRAN_etemp'
 #lstm_model_name = 'HiroLRAN_alldiiid'
 linear_model_name = 'HiroLRAN_betan6'
 
@@ -41,15 +44,14 @@ lstm_model = prediction_helpers.get_considered_models(config_filename, ensemble=
 linear_model = prediction_helpers.get_considered_models(linear_config_filename, ensemble=False)[0]
 
 x_test, y_test, shots, times =customDatasetMakers.ian_dataset(data_filename,profiles,parameters,calculations,actuators,sort_by_size=True)
-
-shot_index = 10
+shot_index = 50
 wanted_sample = x_test[shot_index]
 nwarmup = 3
 starting_index = 0
 end_index = len(wanted_sample) - nwarmup
 
 N = 10 # prediction horizon
-nsim = 250 # number of simulated timesteps
+nsim = 100 # number of simulated timesteps
 
 # get the simulator indices of the states and actuators that I want to control
 future_controller_indices = [len(profiles)*33 + len(parameters) + len(actuators) + actuators.index(controller_actuators[i]) for i in range(len(controller_actuators))]
@@ -58,15 +60,15 @@ initial_state_index_list = [profiles.index(profiles[i]) for i in range(len(contr
 state_indices = [num * 33 + i for num in initial_state_index_list for i in range(33)]
 parameter_indices = [len(profiles)*33 + parameters.index(controller_parameters[i]) for i in range(len(controller_parameters))]
 state_indices = state_indices + parameter_indices
-
 # the state that I alter throughout the simulation
 simulated_state = wanted_sample[starting_index:end_index, :].clone()
 simulated_state = torch.unsqueeze(simulated_state, 0).float()
 simulated_z_t = linear_model.encoder(simulated_state[:,nwarmup,state_indices].clone()).detach().numpy()
 
-target_params = wanted_sample[starting_index:end_index,state_indices].clone()
+# set target state as experimental state
+target_params = wanted_sample[starting_index:end_index, state_indices].clone() 
 target_params = torch.unsqueeze(target_params, 0).float()
-target_params = target_params * 0 + 3
+#target_params = target_params * 0 + 3
 target_z_t = linear_model.encoder(target_params).detach().numpy()
 
 Q_weights = np.ones(latent_dim)
@@ -76,8 +78,8 @@ for i in range(latent_dim):
 Q = csr_matrix(Q)
 
 QN = Q
-R_array = np.eye(len(controller_actuators))*0.1
-#R_array[controller_actuators.index('D_tot'), controller_actuators.index('D_tot')] = 0.01
+R_array = np.eye(len(controller_actuators))*0.01
+#R_array[controller_actuators.index('D_tot'), controller_actuators.index('D_tot')] = 1
 R = csr_matrix(R_array)
 
 # - linear constraints
@@ -87,10 +89,10 @@ xmin = np.array([-np.inf]*latent_dim)
 xmax = np.array([np.inf]*latent_dim)
 
 # set actuators that I don't want to control
-#blocked_actuators = ['tinj', 'ip', 'bt', 'D_tot', 'ech_pwr_total', 'tribot_EFIT01', 'tritop_EFIT01', 'kappa_EFIT01', 'aminor_EFIT01', 'volume_EFIT01', 'rmaxis_EFIT01']
-blocked_actuators = []
+blocked_actuators = ['tinj', 'ip', 'bt','tribot_EFIT01', 'tritop_EFIT01', 'kappa_EFIT01', 'aminor_EFIT01', 'volume_EFIT01', 'rmaxis_EFIT01']
+#blocked_actuators = []
 for actuator in blocked_actuators:
-    true_value = wanted_sample[starting_index, future_controller_indices[controller_actuators.index(actuator)]].clone().item()
+    true_value = wanted_sample[starting_index+nwarmup, current_controller_indices[controller_actuators.index(actuator)]].clone().item()
     umin[controller_actuators.index(actuator)] = true_value
     umax[controller_actuators.index(actuator)] = true_value
 
@@ -115,8 +117,7 @@ u = np.hstack([ueq, uineq])
 
 prob = osqp.OSQP()
 prob.setup(P, q, A, l, u, warm_start=True, max_iter=10000)
-controlled_profiles = []
-actuator_trajectory = []
+
 latent_trajectory = []
 
 for i in range(nsim):
@@ -136,21 +137,18 @@ for i in range(nsim):
     #print(res.x)
 
     # change future actuator, and the current actuator of future timestep
-    simulated_state[0,nwarmup+i,future_controller_indices] = ctrl
-    simulated_state[0, nwarmup+i+1, current_controller_indices] = ctrl
+    simulated_state[0,nwarmup+i-1,future_controller_indices] = ctrl
+    simulated_state[0, nwarmup+i, current_controller_indices] = ctrl
+    predicted_state = prediction_helpers.get_fast_profile_prediction(simulated_state[:,:nwarmup + i, :], lstm_model)
+    if nwarmup + i < len(wanted_sample): # if I'm not at the end of the simulation
+        simulated_state[:, nwarmup + i, 0:len(profiles)*33 + len(parameters)] = predicted_state
 
-    predicted_state = prediction_helpers.get_fast_profile_prediction(simulated_state[:,:nwarmup + i + 1, :], lstm_model)
-    if nwarmup + i + 1 < len(wanted_sample):
-        simulated_state[:, nwarmup + i + 1, state_indices] = predicted_state
-    
-    controlled_profiles.append(predicted_state.detach().numpy())
-    actuator_trajectory.append(ctrl.detach().numpy())
-
-    predicted_z_t = linear_model.encoder(predicted_state).detach().numpy()
+    predicted_z_t = linear_model.encoder(predicted_state[:, :, state_indices]).detach().numpy()
     latent_trajectory.append(predicted_z_t)
-    # Update limits
+
+    # Update limits for blocked actuators
     for actuator in blocked_actuators:
-        true_value = wanted_sample[starting_index+i, future_controller_indices[controller_actuators.index(actuator)]].clone().item()
+        true_value = wanted_sample[starting_index+i+nwarmup, future_controller_indices[controller_actuators.index(actuator)]].clone().item()
         umin[controller_actuators.index(actuator)] = true_value
         umax[controller_actuators.index(actuator)] = true_value
 
@@ -168,14 +166,45 @@ for i in range(nsim):
     prob.update(l=l, u=u, q=q)
     print(f'timestep {i} done')
 
-output_dict = {}
-real_state = wanted_sample[starting_index:end_index,:].clone()
+output_dict = {
+    'real': {},
+    'controlled': {}
+}
+
+prediction_length = end_index - starting_index - nwarmup
+# got to input the correct times here
+true_actuator_trajectory = get_ml_actuator_trajectory(x_test[shot_index:shot_index+1], profiles, parameters, calculations, actuators, nwarmup=nwarmup, prediction_length=prediction_length) # nwarmup and full prediction actuators
+true_warmup_profiles, true_warmup_parameters = get_ml_profile_warmup(x_test[shot_index:shot_index+1], profiles, parameters, calculations, actuators, recorded_profiles=profiles, recorded_parameters=parameters, nwarmup=nwarmup) 
+true_profiles, true_parameters = get_ml_truth(target_params, profiles, parameters, calculations, nwarmup=nwarmup, prediction_length=prediction_length) # get true state at t+1
+
+controlled_actuator_trajectory = get_ml_actuator_trajectory(simulated_state, profiles, parameters, calculations, actuators, nwarmup=nwarmup, prediction_length=prediction_length)
+controlled_warmup_profiles, controlled_warmup_parameters = get_ml_profile_warmup(simulated_state, profiles, parameters, calculations, actuators, recorded_profiles=profiles, recorded_parameters=parameters, nwarmup=nwarmup)
+controlled_profiles, controlled_parameters = get_ml_truth(simulated_state, profiles, parameters, calculations, nwarmup=nwarmup, prediction_length=prediction_length)
+
+combined_true_profiles = np.concatenate((true_warmup_profiles, true_profiles), axis=2)
+combined_controlled_profiles = np.concatenate((controlled_warmup_profiles, controlled_profiles), axis=2)
+combined_true_params = np.concatenate((true_warmup_parameters, true_parameters), axis=2)
+combined_predicted_params = np.concatenate((controlled_warmup_parameters, controlled_parameters), axis=2)
+
+output_dict['real']['profiles'] = combined_true_profiles
+output_dict['real']['parameters'] = combined_true_params
+output_dict['real']['actuators'] = true_actuator_trajectory
+output_dict['controlled']['profiles'] = combined_controlled_profiles
+output_dict['controlled']['parameters'] = combined_predicted_params
+output_dict['controlled']['actuators'] = controlled_actuator_trajectory
+output_dict['time'] = np.arange(len(true_actuator_trajectory[0,0,:]))*20
+
+import pickle
+with open(f'control_pickles/improved{lstm_model_name}{linear_model_name}{shot_index}.pkl', 'wb') as file:
+    # Pickle the array and write it to the file
+    pickle.dump(output_dict, file)
+
+'''controlled_dict = customDatasetMakers.state_to_dic(controlled_state, profiles, parameters, actuators=actuators)
+controlled_denormed_dict = get_denormalized_dic(controlled_dict)
+
+real_state = wanted_sample[starting_index + nwarmup + 1:end_index,:].clone()
 real_dict = customDatasetMakers.state_to_dic(real_state, profiles, parameters, actuators=actuators)
 real_denormed_dict = get_denormalized_dic(real_dict)
-
-controlled_state = simulated_state
-controlled_dict = customDatasetMakers.state_to_dic(controlled_state, controller_profiles, controller_parameters, actuators=controller_actuators)
-controlled_denormed_dict = get_denormalized_dic(controlled_dict)
 
 output_dict['real'] = real_denormed_dict
 output_dict['controlled'] = controlled_denormed_dict
@@ -185,3 +214,4 @@ import pickle
 with open(f'control_pickles/{lstm_model_name}{linear_model_name}{shot_index}.pkl', 'wb') as file:
     # Pickle the array and write it to the file
     pickle.dump(output_dict, file)
+'''
